7ca694e9da2cf3bc675ce631d9519fd8495ba533
CatsOnGitHub <51202885+CatsOnGitHub@users.noreply.github.com>
2020-01-06 15:33:51 +0700

    sua lai view

--- DACK-PTUDW/index.js
+++ DACK-PTUDW/index.js
@@ -1,117 +1,47 @@
- let express = require('express');
- let app = express();
+let express = require('express');
+let app = express();
 
-// set public static folder
+//Set public static folder
 app.use(express.static(__dirname + '/public'));
 
-//Use view Engine
-let expressHandleBar = require('express-handlebars');
-let hbs = expressHandleBar.create({
+//Use view engine
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
     extname: 'hbs',
     defaultLayout: 'layout',
     layoutsDir: __dirname + '/views/layouts/',
-    partialsDir: __dirname + '/views/partials/',    
+    partialsDir: __dirname + '/views/partials/'
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-
-//Define routes
+//Define your routes here 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+app.get('/single-product',(req,res)=>{
+    res.render('product_detail')
+});
+app.get('/categories',(req,res)=>{
+    res.render('allProduct')
+});
+app.get('/:page', (req, res) => {
+    let banners = {
+        blog: 'Our Blog',
+        cart: 'Shopping Cart',
+        category: 'Shop Category',
+        checkout: 'Product Checkout',
+        confirmation: 'Order Confirmation',
+        contact: 'Contact Us',
+        login: 'Login / Register',
+        register: 'Register'
+    }
+    let page = req.params.page;
+    res.render(page, {banner: banners[page]});
+});
 
-app.get('/sign-in', (req, res) => {
-    res.render('sign_in');
-})
-
-app.get('/user-approve', (req,res) => {
-    res.render('approveduser');
-})
-
-app.get('/Auction-Cart', (req,res) => {
-    res.render('AuctionCart');
-})
-
-app.get('/Bidding-Product', (req,res) => {
-    res.render('biddingProducts');
-})
-
-app.get('/Cellphone', (req,res) => {
-    res.render('cellphone');
-})
-
-app.get('/Edit-Profile', (req,res) => {
-    res.render('EditpersonalArea');
-})
-
-app.get('/Edit-Profile-Admin', (req,res) => {
-    res.render('EditpersonalAreaAdmin');
-})
-
-app.get('/Favourite-List', (req,res) => {
-    res.render('favourite');
-})
-
-app.get('/Headphone', (req,res) => {
-    res.render('headphone');
-})
-
-app.get('/Ipad', (req,res) => {
-    res.render('ipad');
-})
-
-app.get('/Category-List', (req,res) => {
-    res.render('listcategory');
-})
-
-app.get('/Product-List', (req,res) => {
-    res.render('listproduct');
-})
-
-app.get('/User-List', (req,res) => {
-    res.render('listuseradmin');
-})
-
-app.get('/My-Profile', (req,res) => {
-    res.render('personalArea');
-})
-
-app.get('/Admin-Profile', (req,res) => {
-    res.render('personalAreaAdmin');
-})
-
-app.get('/Personal-Profile', (req,res) => {
-    res.render('personalProfile');
-})
-
-app.get('/Product-Detail', (req,res) => {
-    res.render('product_detail');
-})
-
-app.get('/Refuse', (req,res) => {
-    res.render('refuse');
-})
-
-app.get('/Seller', (req,res) => {
-    res.render('seller');
-})
-
-app.get('/Shoes', (req,res) => {
-    res.render('shoes');
-})
-
-app.get('/Sign-up', (req,res) => {
-    res.render('sign_up');
-})
-
-app.get('/Bidded-Product', (req,res) => {
-    res.render('wonProducts');
-})
-
-app.use
- //set Server port and Start server
- app.set('port', process.env.PORT || 4000);
- app.listen(app.get('port'), () => {
+//Set server port & start server
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), () => {
     console.log(`Server is running at port ${app.get('port')}`);
- });
+});
